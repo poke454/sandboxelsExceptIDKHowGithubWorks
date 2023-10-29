85672321e54ca7fcfb6c596b105eeba7e5363b91
poke454 <jordanbell59@gmail.com>
2023-10-29 10:42:34 -0500

    first attempt to add primordial soup reaction
    a mod to create primordial soup from elements which are available in the Alchemy mod.

---
+++ mods/abiogenesis.js
@@ -0,0 +1,4 @@
+if (!elements.dirty_water.reactions) { // Include this block once
+    elements.dirty_water.reactions = {} // This creates the property if it doesn't exist
+}
+elements.dirty_water.reactions.wet_sand = { "elem1":null, "elem2":"primordial_soup" }
